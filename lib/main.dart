d167a1729475b895c8952489d17c35994a1bdc51
Felipe Mourao <felipepcmourao@gmail.com>
2025-12-11 10:54:50 +0000

    changes of color

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.red),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Felipe Demo Home Page'),
     );
   }
 }
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Você clicou esse tanto de vezes:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
